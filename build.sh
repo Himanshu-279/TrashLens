f468cfa9717ff4d6bffab60a6be0ab586455dfa7
Himanshu Verma <151406549+Himanshu-279@users.noreply.github.com>
2025-11-30 12:16:51 +0530

    Clean up build.sh by removing unnecessary lines
    Removed shebang and comments from build script

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
-# exit on error
+
 set -o errexit
 
-# Sirf packages install karo aur static files collect karo
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
